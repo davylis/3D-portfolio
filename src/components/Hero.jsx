ed9f76020cfbd0ee7fc10c239e09531631b29619
Liisa Davydov <liisamuttonen@gmail.com>
2025-08-15 00:46:32 +0300

    Modify App file, fix tailwindcss and add reactrouter

---
+++ src/components/Hero.jsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <div className="bg-green-500 text-white p-8">Hero Section</div>;
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div className="bg-blue-500 text-white p-4">Navbar</div>;
+};
+
+export default Navbar;
