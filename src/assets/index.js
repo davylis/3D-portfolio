a9a29e348abc5fd1b4b61988f199d21e314fdd19
Liisa Davydov <liisamuttonen@gmail.com>
2025-08-15 18:26:07 +0300

    Add logo, menu and etc. Also modify Navbar and started to build it

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+import logo from './logo.png';
+import menu from './menu.svg';
+import close from './close.svg';
+import meta from './company/meta.png'
+
+
+export {
+  logo,
+  menu,
+  close,
+  meta,
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
 
 
 const Navbar = () => {
-  return <div className="bg-blue-500 text-white p-4">Navbar</div>;
+    const [active, setActive] = useState('');
+  return (
+    <nav
+        className={
+            //special utility class for padding
+            `${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+            >
+                <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+                    <Link
+                    to="/"
+                    className="flex items-center gap-2"
+                    onClick={() => {
+                        setActive("");
+                        window.scrollTo(0, 0)
+                    }}
+                    >
+                        <img src={logo} alt="logo" className="w-21 h-14 object-conatin"/>
+                    </Link>
+                </div>
+    </nav>
+  )
 };
 
 export default Navbar;
