b2768a1ef428de07abb7d50fba1bafe17f311ad3
Liisa Davydov <liisamuttonen@gmail.com>
2025-08-24 00:07:07 +0300

    Add gradient icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,17 +2,49 @@ import React from "react";
 import { logo } from "../assets";
 import { FaServer } from "react-icons/fa";
 import { FaCode } from "react-icons/fa";
+import { BiCodeAlt } from "react-icons/bi";
 import Model1 from "./canvas/Model1";
 
 const About = () => {
   return (
     <section className="skills-section">
       <h1 className="section-title autoDisplay">My Skills 💪</h1>
+     
+
       <div className="skills-box autoDisplay min-h-[500px]">
         <Model1/>
         <div className="FrontEnd">
           <h1 className="gradient">
-            FrontEnd <FaCode />
+            FrontEnd 
+            <svg width="64" height="64" viewBox="0 0 24 24">
+      <defs>
+        <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
+          <stop offset="0%">
+            <animate
+              attributeName="stop-color"
+              values="#8B5CF6;#EC4899;#8B5CF6"
+              dur="6s"
+              repeatCount="indefinite"
+            />
+          </stop>
+          <stop offset="100%">
+            <animate
+              attributeName="stop-color"
+              values="#EC4899;#8B5CF6;#EC4899"
+              dur="6s"
+              repeatCount="indefinite"
+            />
+          </stop>
+        </linearGradient>
+      </defs>
+
+      {/* bx-code-alt path */}
+      <path
+        fill="url(#grad)"
+        d="M9.414 16l-4.707-4.707 4.707-4.707 1.414 1.414L7.242 11.5l3.586 3.586zM14.586 
+           16l-1.414-1.414L16.758 11.5l-3.586-3.586 1.414-1.414L21.414 11.5z"
+      />
+    </svg>
           </h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -26,8 +58,41 @@ const About = () => {
         <div className="BackEnd">
           <h1 className="gradient gap-2 flex-items-center">
             BackEnd
-            <svg width="0" height="0"></svg>
-            <FaServer />
+                <svg
+      width="64"
+      height="64"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <linearGradient id="gradServer" x1="0%" y1="0%" x2="100%" y2="0%">
+          <stop offset="0%">
+            <animate
+              attributeName="stop-color"
+              values="#8B5CF6; #EC4899; #8B5CF6"
+              dur="6s"
+              repeatCount="indefinite"
+            />
+          </stop>
+          <stop offset="100%">
+            <animate
+              attributeName="stop-color"
+              values="#EC4899; #8B5CF6; #EC4899"
+              dur="6s"
+              repeatCount="indefinite"
+            />
+          </stop>
+        </linearGradient>
+      </defs>
+
+      {/* Server Icon Path from Boxicons */}
+      <path
+        fill="url(#gradServer)"
+        d="M4 3h16a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2V5a2 2 0 012-2zm0 
+           10h16a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4a2 2 0 012-2z
+           M6 7a1 1 0 100 2 1 1 0 000-2zm0 10a1 1 0 100 2 1 1 0 000-2z"
+      />
+    </svg>
           </h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
