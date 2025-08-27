d87092885607b9699a0237bcb11c3a705fa99918
Liisa Davydov <liisamuttonen@gmail.com>
2025-08-27 21:35:16 +0300

    migrate to react@18.3.1 from react@19

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,51 +1,50 @@
 import React from "react";
-import { logo } from "../assets";
 import { FaServer } from "react-icons/fa";
 import { FaCode } from "react-icons/fa";
 import { BiCodeAlt } from "react-icons/bi";
 import Model1 from "./canvas/Model1";
-import { css, aws, c, docker, git, html, java, js, linux, mysql, postman, python, react, tailwind } from "../assets/tech";
+import { css, aws, c, git, html, java, js, linux, mysql, postman, python, react, tailwind } from "../assets/tech";
 
 const About = () => {
   return (
     <section className="skills-section">
       <h1 className="section-title autoDisplay">My Skills 💪</h1>
-     
+
 
       <div className="skills-box autoDisplay">
-        <Model1/>
+        <Model1 />
         <div className="FrontEnd">
           <h1 className="gradient">
-            FrontEnd 
+            FrontEnd
             <svg width="64" height="64" viewBox="0 0 24 24">
-      <defs>
-        <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop offset="0%">
-            <animate
-              attributeName="stop-color"
-              values="#8B5CF6;#EC4899;#8B5CF6"
-              dur="6s"
-              repeatCount="indefinite"
-            />
-          </stop>
-          <stop offset="100%">
-            <animate
-              attributeName="stop-color"
-              values="#EC4899;#8B5CF6;#EC4899"
-              dur="6s"
-              repeatCount="indefinite"
-            />
-          </stop>
-        </linearGradient>
-      </defs>
+              <defs>
+                <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
+                  <stop offset="0%">
+                    <animate
+                      attributeName="stop-color"
+                      values="#8B5CF6;#EC4899;#8B5CF6"
+                      dur="6s"
+                      repeatCount="indefinite"
+                    />
+                  </stop>
+                  <stop offset="100%">
+                    <animate
+                      attributeName="stop-color"
+                      values="#EC4899;#8B5CF6;#EC4899"
+                      dur="6s"
+                      repeatCount="indefinite"
+                    />
+                  </stop>
+                </linearGradient>
+              </defs>
 
-      {/* bx-code-alt path */}
-      <path
-        fill="url(#grad)"
-        d="M9.414 16l-4.707-4.707 4.707-4.707 1.414 1.414L7.242 11.5l3.586 3.586zM14.586 
+              {/* bx-code-alt path */}
+              <path
+                fill="url(#grad)"
+                d="M9.414 16l-4.707-4.707 4.707-4.707 1.414 1.414L7.242 11.5l3.586 3.586zM14.586 
            16l-1.414-1.414L16.758 11.5l-3.586-3.586 1.414-1.414L21.414 11.5z"
-      />
-    </svg>
+              />
+            </svg>
           </h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -59,41 +58,41 @@ const About = () => {
         <div className="BackEnd">
           <h1 className="gradient gap-2 flex-items-center">
             BackEnd
-                <svg
-      width="64"
-      height="64"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <defs>
-        <linearGradient id="gradServer" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop offset="0%">
-            <animate
-              attributeName="stop-color"
-              values="#8B5CF6; #EC4899; #8B5CF6"
-              dur="6s"
-              repeatCount="indefinite"
-            />
-          </stop>
-          <stop offset="100%">
-            <animate
-              attributeName="stop-color"
-              values="#EC4899; #8B5CF6; #EC4899"
-              dur="6s"
-              repeatCount="indefinite"
-            />
-          </stop>
-        </linearGradient>
-      </defs>
+            <svg
+              width="64"
+              height="64"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <linearGradient id="gradServer" x1="0%" y1="0%" x2="100%" y2="0%">
+                  <stop offset="0%">
+                    <animate
+                      attributeName="stop-color"
+                      values="#8B5CF6; #EC4899; #8B5CF6"
+                      dur="6s"
+                      repeatCount="indefinite"
+                    />
+                  </stop>
+                  <stop offset="100%">
+                    <animate
+                      attributeName="stop-color"
+                      values="#EC4899; #8B5CF6; #EC4899"
+                      dur="6s"
+                      repeatCount="indefinite"
+                    />
+                  </stop>
+                </linearGradient>
+              </defs>
 
-      {/* Server Icon Path from Boxicons */}
-      <path
-        fill="url(#gradServer)"
-        d="M4 3h16a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2V5a2 2 0 012-2zm0 
+              {/* Server Icon Path from Boxicons */}
+              <path
+                fill="url(#gradServer)"
+                d="M4 3h16a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2V5a2 2 0 012-2zm0 
            10h16a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4a2 2 0 012-2z
            M6 7a1 1 0 100 2 1 1 0 000-2zm0 10a1 1 0 100 2 1 1 0 000-2z"
-      />
-    </svg>
+              />
+            </svg>
           </h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -105,7 +104,7 @@ const About = () => {
         </div>
 
         <div
-          className="slider" 
+          className="slider"
           reverse="true"
           style={{
             "--width": "100px",
@@ -118,40 +117,40 @@ const About = () => {
               <img src={html} alt="" />
             </div>
             <div className="item" style={{ "--position": 2 }}>
-              <img  src={css} alt="" />
+              <img src={css} alt="" />
             </div>
             <div className="item" style={{ "--position": 3 }}>
-              <img  src={git} alt="" />
+              <img src={git} alt="" />
             </div>
             <div className="item" style={{ "--position": 4 }}>
-              <img  src={aws} alt="" />
+              <img src={aws} alt="" />
             </div>
             <div className="item" style={{ "--position": 5 }}>
-              <img  src={java} alt="" />
+              <img src={java} alt="" />
             </div>
             <div className="item" style={{ "--position": 6 }}>
-              <img  src={c} alt="" />
+              <img src={c} alt="" />
             </div>
             <div className="item" style={{ "--position": 7 }}>
-              <img  src={js} alt="" />
+              <img src={js} alt="" />
             </div>
             <div className="item" style={{ "--position": 8 }}>
-              <img  src={linux} alt="" />
+              <img src={linux} alt="" />
             </div>
             <div className="item" style={{ "--position": 9 }}>
-              <img  src={mysql} alt="" />
+              <img src={mysql} alt="" />
             </div>
             <div className="item" style={{ "--position": 10 }}>
-              <img  src={postman} alt="" />
+              <img src={postman} alt="" />
             </div>
             <div className="item" style={{ "--position": 11 }}>
-              <img  src={python} alt="" />
+              <img src={python} alt="" />
             </div>
             <div className="item" style={{ "--position": 12 }}>
-              <img  src={react} alt="" />
+              <img src={react} alt="" />
             </div>
             <div className="item" style={{ "--position": 13 }}>
-              <img  src={tailwind} alt="" />
+              <img src={tailwind} alt="" />
             </div>
           </div>
         </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,59 @@
 import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { textVariant } from '../utils/motion';
+// eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react"
+import { experiences } from '../constants';
+import "react-vertical-timeline-component/style.min.css";
+import { styles } from '../styles';
+import  SectionWrapper  from '../hoc/SectionWrapper'
 
+
+// eslint-disable-next-line react-refresh/only-export-components
+const ExperienceCard = ({ experience }) => (
+  <VerticalTimelineElement contentStyle={{ background: '#1d1836', color: '#fff'}}
+  contentArrowStyle={{ borderRight: '7 px solid #232631'}}
+  date={experience.date}
+  iconStyle={{background: experience.iconBg }}
+  icon={
+    <div className='flex justify-center items-center w-full h-full'>
+      <img
+      src={experience.icon}
+            alt={experience.company_name}
+            className='w-[60%] h-[60%] object-contain'
+          />
+      </div>
+  }
+   >
+  </VerticalTimelineElement>
+)
+
+// eslint-disable-next-line react-refresh/only-export-components
 const Experience = () => {
   return (
-    <section className="py-16 px-8" id="experience">
-      <div className="max-w-4xl mx-auto text-center">
-        <h2 className="text-4xl font-bold text-gray-800 mb-4">Experience</h2>
-        <p className="text-gray-600 text-lg leading-relaxed">
-          Here is a list of my professional experiences and projects I have worked on.
+    <>
+     <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far
         </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Work Experience.
+        </h2>
+      </motion.div>
+
+       <div className='mt-20 flex flex-col'>
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            />
+          ))}
+        </VerticalTimeline>
       </div>
-    </section>
+      </>
   );
 };
 
-export default Experience;
+// eslint-disable-next-line react-refresh/only-export-components
+export default SectionWrapper(Experience, "work");
